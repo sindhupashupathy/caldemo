55184d4a7834187f3109180fd3785b258d0aaef9
sindh <sindh@LAPTOP-14FJVSOH.home>
2019-06-17 12:38:35 +0200

    new project

---
+++ Caldemo/src/main/java/com/sindhu/Caldemo/Caldemo.java
@@ -0,0 +1,14 @@
+package com.sindhu.Caldemo;
+
+public class Caldemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c = a +b ;
+		System.out.println("value is : "+c);
+
+	}
+
+}
